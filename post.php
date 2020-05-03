d205e1f100818aea1df782bd93fbc6b8e00f3c48
Alexander <shurpo.alexander@gmail.com>
2020-05-04 00:52:21 +0300

    мелкие правки

--- post.php
+++ post.php
@@ -15,4 +15,5 @@
   $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
   $headers .= "From: Отправитель <from@example.com>\r\n"; //Наименование и почта отправителя
   mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+  print_r($_POST, true)
 ?>
